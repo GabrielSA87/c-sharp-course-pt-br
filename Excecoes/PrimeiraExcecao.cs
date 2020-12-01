d6c0df0537fd0a0052e33a0d3e6e05b1772d43a5
Gabriel Araujo <72812005+GabrielSA87@users.noreply.github.com>
2020-12-01 13:47:11 -0300

    Add files via upload

---
+++ Excecoes/PrimeiraExcecao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Excecoes
+{
+    public class Conta
+    {
+        double Saldo;
+
+        public Conta(double saldo)
+        {
+            Saldo = saldo;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor > Saldo)
+            {
+                throw new ArgumentException("Saldo insuficiente.");
+            }
+
+            Saldo -= valor;
+        }
+    }
+    
+    class PrimeiraExcecao
+    {
+        public static void Executar()
+        {            
+            var conta = new Conta(1223.45);
+
+            try
+            {
+                conta.Sacar(1_600);
+                Console.WriteLine("Saque com sucesso!");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Obrigado!");                
+            }
+        }
+    }
+}
